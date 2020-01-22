a54bf544358d8e1a73339f8018d3a15c4dba8433
niq77avie@yahoo.com <niq77avie@yahoo.com>
2020-01-22 22:52:55 +0800

    1

---
+++ src/main/java/com/optum/ocr/repository/LoginHistory.java
@@ -0,0 +1,26 @@
+package com.optum.ocr.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "LoginHistory")
+public class LoginHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "LoginHistoryId")
+    public Long LoginHistoryId;
+
+    public String msId;
+    public LocalDate lastLogin;
+}
